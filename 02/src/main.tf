42c910a183b5f99f01d352bc3463b8dcdbc0a391
EvgeniyaBalanyuk <62482983+EvgeniyaBalanyuk@users.noreply.github.com>
2024-10-16 13:38:04 +0300

    Update main.tf

--- 02/src/main.tf
+++ 02/src/main.tf
@@ -1,6 +1,9 @@
+#создаем облачную сеть develop
 resource "yandex_vpc_network" "develop" {
   name = var.vpc_name
 }
+
+#создаем облачную подсеть zone A develop-web
 resource "yandex_vpc_subnet" "develop" {
   name           = var.vpc_name
   zone           = var.default_zone
@@ -8,34 +11,100 @@ resource "yandex_vpc_subnet" "develop" {
   v4_cidr_blocks = var.default_cidr
 }
 
+#создаем облачную подсеть zone B develop-db
+resource "yandex_vpc_subnet" "develop_b" {
+  name           = var.vpc_db_name
+  zone           = var.vpc_db_zone
+  network_id     = yandex_vpc_network.develop.id
+  v4_cidr_blocks = var.vpc_db_cidr
+}
+
+# 1 VM
 
 data "yandex_compute_image" "ubuntu" {
-  family = "ubuntu-2004-lts"
+  family = var.vm_web_image_family
 }
-resource "yandex_compute_instance" "platform" {
-  name        = "netology-develop-platform-web"
-  platform_id = "standart-v4"
+
+resource "yandex_compute_instance" "platform_web" {
+  name            = var.vm_web_name
+  hostname        = var.vm_web_name
+  platform_id     = var.vm_web_platform_id
+  zone            = var.vpc_web_zone
+
   resources {
-    cores         = 1
-    memory        = 1
-    core_fraction = 5
+    cores          = var.vm_web_cores
+    memory         = var.vm_web_memory
+    core_fraction  = var.vm_web_core_fraction
   }
+
   boot_disk {
     initialize_params {
       image_id = data.yandex_compute_image.ubuntu.image_id
     }
   }
+
   scheduling_policy {
-    preemptible = true
+    preemptible = var.vm_web_preemptible
   }
+
   network_interface {
-    subnet_id = yandex_vpc_subnet.develop.id
-    nat       = true
+    subnet_id       = yandex_vpc_subnet.develop.id
+    nat              = var.vm_web_nat
   }
 
   metadata = {
-    serial-port-enable = 1
+    serial-port-enable = var.vm_web_serial_port_enable
     ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
   }
 
 }
+
+# 2 VM
+
+data "yandex_compute_image" "ubuntu_db" {
+  family = var.vm_db_image_family
+}
+
+resource "yandex_compute_instance" "platform_db" {
+  name        = var.vm_db_name
+  platform_id = var.vm_db_platform_id
+  zone        = var.vpc_db_zone
+
+  resources {
+    cores         = var.vm_db_cores
+    memory        = var.vm_db_memory
+    core_fraction = var.vm_db_core_fraction
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu_db.image_id
+    }
+  }
+
+  scheduling_policy {
+    preemptible = var.vm_db_preemptible
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop_b.id
+    nat       = var.vm_db_nat
+  }
+
+  metadata = {
+    serial-port-enable = var.vm_db_serial_port_enable
+    ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
+  }
+
+}
+
+/*
+output "test" {
+
+  value = [
+    { dev1 = ["ssh -o 'StrictHostKeyChecking=no' ubuntu@${yandex_compute_instance.platform_web.network_interface[0].nat_ip_address}", yandex_compute_instance.platform_web.network_interface[0].ip_address] },
+    { dev2 = ["ssh -o 'StrictHostKeyChecking=no' ubuntu@${yandex_compute_instance.platform_db.network_interface[0].nat_ip_address}", yandex_compute_instance.platform_db.network_interface[0].ip_address] }
+
+  ]
+}
+*/
