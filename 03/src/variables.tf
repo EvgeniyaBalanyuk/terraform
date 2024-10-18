f8de11f6a89cb97dbfc18fdbc080d287a2a5df8c
EvgeniyaBalanyuk <62482983+EvgeniyaBalanyuk@users.noreply.github.com>
2024-10-18 08:59:41 +0300

    Update variables.tf

--- 03/src/variables.tf
+++ 03/src/variables.tf
@@ -1,16 +1,19 @@
 ###cloud vars
 variable "token" {
   type        = string
+  default     = "y0_AgAAAABIRaajAATuwQAAAAEU8YMTAACs4IDPGz5I7KMP-ZAsWyiLW_dj6g"
   description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
 }
 
 variable "cloud_id" {
   type        = string
+  default     = "b1gjmpd4ofucqn84e7lj"
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
 }
 
 variable "folder_id" {
   type        = string
+  default     = "b1ga3emslagphg3662qr"
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
 }
 
@@ -29,4 +32,41 @@ variable "vpc_name" {
   type        = string
   default     = "develop"
   description = "VPC network&subnet name"
+}
+
+### VM variables
+variable "web_vm_cpu" {
+  type        = number
+  default     = 2
+  description = "Количество CPU для web ВМ"
+}
+
+variable "web_vm_ram" {
+  type        = number
+  default     = 4
+  description = "Объем RAM для web ВМ (в ГБ)"
+}
+
+variable "web_vm_disk_size" {
+  type        = number
+  default     = 10
+  description = "Размер диска для web ВМ (в ГБ)"
+}
+
+variable "web_vm_image_id" {
+  type        = string
+  default     = "fd83b9pkhhr6m7tegqjm"
+  description = "ID образа для web ВМ"
+}
+
+variable "db_vm_image_id" {
+  type        = string
+  default     = "fd83b9pkhhr6m7tegqjm"
+  description = "ID образа для ВМ баз данных"
+}
+
+variable "ssh_key_path" {
+  type        = string
+  default     = "~/.ssh/id_ed25519.pub"
+  description = "Путь до SSH-ключа"
 }
